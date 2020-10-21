34e287b7d965b3ecf47a0818757bc5ef2782fed4
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-10-21 18:47:30 +0900

    :fire: Remove unused

--- src/register.js
+++ src/register.js
@@ -2,18 +2,9 @@
 import React from 'react';
 import { addons, types } from '@storybook/addons';
 import Tool from './Tool';
-import { IconButton } from '@storybook/components';
 
 const ADDON_ID = 'myaddon';
 const PANEL_ID = `${ADDON_ID}/panel`;
-
-// const onClickIconButton = ({ api }) => {
-//   Store.setIsDarkMode(!Store.getIsDarkMode());
-//   addons.setConfig({
-//     theme: Store.getIsDarkMode() ? themes.dark : themes.light,
-//   });
-//   console.log(api);
-// };
 
 addons.register(ADDON_ID, (api) => {
   addons.add(PANEL_ID, {
