c51dadc24bf56b83bda10064efb678c738ee109b
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-10-21 19:09:35 +0900

    :sparkles: Implment setConfig

--- src/register.js
+++ src/register.js
@@ -2,9 +2,14 @@
 import React from 'react';
 import { addons, types } from '@storybook/addons';
 import Tool from './Tool';
+import { themes } from '@storybook/theming';
 
 const ADDON_ID = 'myaddon';
 const PANEL_ID = `${ADDON_ID}/panel`;
+
+addons.setConfig({
+  theme: themes.light,
+});
 
 addons.register(ADDON_ID, (api) => {
   addons.add(PANEL_ID, {
