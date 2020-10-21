788dc21a96ee1336709d41921306be77e8640ee5
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-10-21 19:09:48 +0900

    :construction: Work in progress

--- src/Tool.js
+++ src/Tool.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { IconButton } from '@storybook/components';
+import { themes } from '@storybook/theming';
+import {
+  STORY_CHANGED,
+  SET_STORIES,
+  DOCS_RENDERED,
+} from '@storybook/core-events';
 
-const { useState } = React;
+const { useState, useEffect, useCallback } = React;
 
 function DarkMode({ api }) {
   // inital state should always be false;
   const [isDarkMode, setDarkMode] = useState(false);
-  
+
+  // const setMode = useCallback((mode) => {}, []);
+
+  const setMode = useCallback(() => {
+    const changedState = !isDarkMode;
+    setDarkMode(changedState);
+    api.setOptions({ themes: changedState ? themes.dark : themes.light });
+    api.getChannel().emit('DARK_MODE_VUE', changedState);
+  });
+
+  useEffect(() => {
+    const channel = api.getChannel();
+    return () => {};
+  });
+
   return (
-    <IconButton active={isDarkMode} onClick={}>{isDarkMode ? 'DARK' : 'LIGHT'}</IconButton>
+    <IconButton active={isDarkMode} onClick={setMode}>
+      {isDarkMode ? 'DARK' : 'LIGHT'}
+    </IconButton>
   );
 }
 
