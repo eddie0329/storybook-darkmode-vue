4601791a5eb6b185ed67eafa678de9702dd45714
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-10-21 16:21:56 +0900

    :sparkles: Implement vue project

---
+++ storybook-darkmode-vue-test/src/stories/Sample.stories.js
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('Sample', module).add('sample1', () => ({
+  data: () => ({}),
+  components: {},
+  methods: {},
+  template: `
+    <div>hello</div>
+  `,
+}));
