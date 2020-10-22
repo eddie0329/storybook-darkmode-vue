a7fa92c7bf3f3d5bd214a7dd83bc558d9f4c072b
eddie0329 <fdsa0329@naver.com>
2020-10-22 21:10:06 +0900

    :rocket: Deploy 1.1.7

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
-
+const pkg = require('./package.json');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'development',
@@ -8,19 +8,22 @@ module.exports = {
     extensions: ['.js'],
   },
   entry: {
-    app: path.join(__dirname, '/src/register.js')
+    app: path.join(__dirname, '/src/register.js'),
   },
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      }
-    ]
+      },
+    ],
   },
   output: {
     filename: 'register.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    library: pkg.name,
+    libraryTarget: 'commonjs2',
   },
-}
+};
