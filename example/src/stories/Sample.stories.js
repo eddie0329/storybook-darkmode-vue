7740317c0a031e2e14dc0fbc44db0f2b0ce97fd9
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-03-18 11:30:54 +0900

    master: :rocket: Deploy 1.1.8

--- example/src/stories/Sample.stories.js
+++ example/src/stories/Sample.stories.js
@@ -1,12 +1,17 @@
 import { storiesOf } from '@storybook/vue';
 import DarkModeWrapper from 'storybook-darkmode-vue/DarkModeWrapper';
 import DarkModeSample from './DarkModeSample';
+import { addons } from '@storybook/addons';
 
 storiesOf('Sample', module).add('sample1', () => ({
   data: () => ({}),
   components: {
     DarkModeWrapper,
     DarkModeSample,
+  },
+  created() {
+    const channel = addons.getChannel();
+    console.log(channel);
   },
   methods: {},
   template: `
